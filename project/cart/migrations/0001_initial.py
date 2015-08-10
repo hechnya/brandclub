396c67f163a77007e7d8b1a4942cc20f93fc4135
hechnya <hechnya@mail.ru>
2015-08-10 11:34:30 +0300

    Рабочий проект удолили базу данных

--- project/cart/migrations/0001_initial.py
+++ project/cart/migrations/0001_initial.py
@@ -18,12 +18,39 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('count', models.IntegerField()),
+                ('cart_id', models.CharField(max_length=240)),
+                ('parametr', models.ForeignKey(to='core.ProductParametr')),
                 ('product', models.ForeignKey(to='core.Product')),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'verbose_name': '\u041e\u0431\u044a\u0435\u043a\u0442 \u043a\u043e\u0440\u0437\u0438\u043d\u044b',
                 'verbose_name_plural': '\u041a\u043e\u0440\u0437\u0438\u043d\u0430',
+            },
+        ),
+        migrations.CreateModel(
+            name='Delivery',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('cart_id', models.CharField(max_length=240)),
+                ('city', models.CharField(max_length=100)),
+                ('price', models.IntegerField()),
+            ],
+            options={
+                'verbose_name': '\u0414\u043e\u0441\u0442\u0430\u0432\u043a\u0430',
+                'verbose_name_plural': '\u0414\u043e\u0441\u0442\u0430\u0432\u043a\u0430',
+            },
+        ),
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('cart_id', models.CharField(max_length=240)),
+                ('is_paid', models.BooleanField(default=False)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': '\u041e\u0431\u044c\u0435\u043a\u0442 \u0437\u0430\u043a\u0430\u0437\u0430',
+                'verbose_name_plural': '\u0417\u0430\u043a\u0430\u0437\u044b',
             },
         ),
     ]
--- project/core/migrations/0001_initial.py
+++ project/core/migrations/0001_initial.py
@@ -18,6 +18,8 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=200)),
                 ('text', models.TextField()),
                 ('date', models.DateField(auto_now_add=True)),
+                ('meta_title', models.CharField(max_length=60, blank=True)),
+                ('meta_description', models.CharField(max_length=150, blank=True)),
             ],
             options={
                 'verbose_name': '\u0421\u0442\u0430\u0442\u044c\u044f',
@@ -42,6 +44,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=200)),
                 ('slug', models.SlugField(max_length=200)),
+                ('meta_title', models.CharField(max_length=60, blank=True)),
+                ('meta_description', models.CharField(max_length=150, blank=True)),
                 ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
@@ -60,6 +64,8 @@ class Migration(migrations.Migration):
                 ('slug', models.SlugField(max_length=150)),
                 ('name', models.CharField(max_length=2000)),
                 ('page', models.TextField()),
+                ('meta_title', models.CharField(max_length=60, blank=True)),
+                ('meta_description', models.CharField(max_length=150, blank=True)),
             ],
             options={
                 'verbose_name': '\u0421\u0442\u0440\u0430\u043d\u0438\u0446\u044b',
@@ -85,7 +91,10 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=150)),
                 ('slug', models.SlugField(unique=True, max_length=150)),
                 ('description', models.TextField()),
-                ('price', models.IntegerField()),
+                ('video', models.CharField(max_length=200, null=True, blank=True)),
+                ('meta_title', models.CharField(max_length=60, blank=True)),
+                ('meta_description', models.CharField(max_length=150, blank=True)),
+                ('article', models.OneToOneField(null=True, blank=True, to='core.Article')),
                 ('category', models.ManyToManyField(to='core.Category')),
             ],
             options={
@@ -98,11 +107,37 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('image', models.ImageField(upload_to=b'product')),
+                ('is_main', models.BooleanField(default=False, verbose_name='\u041e\u0441\u043d\u043e\u0432\u043d\u043e\u0439 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440')),
                 ('product', models.ForeignKey(to='core.Product')),
             ],
             options={
                 'verbose_name': '\u0418\u0437\u043e\u0431\u0440\u0430\u0436\u0435\u043d\u0438\u0435',
                 'verbose_name_plural': '\u0418\u0437\u043e\u0431\u0440\u0430\u0436\u0435\u043d\u0438\u044f \u043f\u0440\u043e\u0434\u0443\u043a\u0442\u043e\u0432',
+            },
+        ),
+        migrations.CreateModel(
+            name='ProductParametr',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('weight', models.IntegerField()),
+                ('price', models.IntegerField()),
+                ('is_main', models.BooleanField(default=False, verbose_name='\u041e\u0441\u043d\u043e\u0432\u043d\u043e\u0439 \u043f\u0430\u0440\u0430\u043c\u0435\u0442\u0440')),
+                ('product', models.ForeignKey(to='core.Product')),
+            ],
+            options={
+                'verbose_name': '\u041f\u0430\u0440\u0430\u043c\u0435\u0442\u0440\u044b \u043f\u0440\u043e\u0434\u0443\u043a\u0442\u0430',
+                'verbose_name_plural': '\u041f\u0430\u0440\u0430\u043c\u0435\u0442\u0440\u044b \u043f\u0440\u043e\u0434\u0443\u043a\u0442\u043e\u0432',
+            },
+        ),
+        migrations.CreateModel(
+            name='Slide',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('image', models.ImageField(upload_to=b'slides')),
+            ],
+            options={
+                'verbose_name': '\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c \u0444\u043e\u0442\u043e \u0432 \u0441\u043b\u0430\u0439\u0434\u0435\u0440',
+                'verbose_name_plural': '\u0424\u043e\u0442\u043e \u0441\u043b\u0430\u0439\u0434 \u0433\u043b\u0430\u0432\u043d\u043e\u0439 \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u044b',
             },
         ),
     ]
